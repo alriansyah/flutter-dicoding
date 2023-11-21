d9907fdbc73a9ed68a48efb17c5048617de3c0b3
alriansyah <alriansyah1218@gmail.com>
2023-11-21 21:47:32 +0700

    Constants and Final

---
+++ Dart/13-ConstantDanFinal.dart
@@ -0,0 +1,10 @@
+const num pi = 3.14;
+final gravity = 9.8;
+
+void main() {
+  var radius = 7;
+  print(
+      'Luas lingkaran dengan radius $radius = ${calculateCircleArea(radius)}');
+}
+
+num calculateCircleArea(num radius) => pi * radius * radius;

065ed0852cc767434e14229a5c5fd87d5221beb5
alriansyah <alriansyah1218@gmail.com>
2023-11-25 06:50:20 +0700

    Dart Future

---
+++ Dart/39-DartFuture.dart
@@ -0,0 +1,37 @@
+void main() {
+  final myFuture = Future(() {
+    print('Creating the future');
+    return 12;
+  });
+  print('main() done');
+
+  // Future pada function getOrder()
+  getOrder().then((value) {
+    print('Your ordered: $value');
+  }).catchError((error) {
+    print('Sorry. $error');
+  }).whenComplete(() {
+    print('Thank you');
+  });
+  print('Getting your order...');
+}
+
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    var isStockAvailable = false;
+    if (isStockAvailable) {
+      return 'Coffee Boba';
+    } else {
+      throw 'Our stock is not enough.';
+    }
+  });
+}
+
+// Fungsi getOrder() yang berisi Future yang masih uncompleted.
+// Method then() yang menangani kondisi completed with data.
+// Method catchError() yang menangani kondisi completed with error.
+/*
+Ada satu method lagi yang bisa kita gunakan yaitu whenComplete(). Method ini akan dijalankan 
+ketika suatu fungsi Future selesai dijalankan, tak peduli apakah menghasilkan nilai atau eror. 
+Ini seperti blok finally pada try-catch-finally.
+*/

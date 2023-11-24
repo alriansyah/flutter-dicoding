7546dfd18ca195c3b144e0a2ef0159f4d4590d4c
alriansyah <alriansyah1218@gmail.com>
2023-11-24 16:48:54 +0700

    Implicit Interface

---
+++ Dart/30-ImplicitInterface.dart
@@ -0,0 +1,12 @@
+import 'lib/bird.dart';
+
+void main() {
+  var dicodingBird = Bird('Birdy', 2, 0.8, 'Gray');
+
+  dicodingBird.fly();
+  dicodingBird.eat();
+}
+
+/// Output:
+/// Birdy is flying
+/// Birdy is eating.
